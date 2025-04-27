723582d7ca130d624a6fc278f02715ee601dc3d0
Sara-Rubaya <sararubaya4800@gmail.com>
2025-04-28 00:01:56 +0600

    added new repo

---
+++ src/App.jsx
@@ -0,0 +1,18 @@
+
+import './App.css'
+import FormsAction from './Component/FormAction/FormsAction'
+import SimpleForm from './Component/SimpleForm/SimpleForm'
+
+function App() {
+ 
+
+  return (
+    <>
+      <h1>Explore  React Form</h1>
+      {/* <SimpleForm></SimpleForm> */}
+      <FormsAction></FormsAction>
+    </>
+  )
+}
+
+export default App
